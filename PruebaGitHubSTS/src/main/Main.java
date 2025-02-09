f90dcd898e071827bb6d0f4c574092f29d476efc
victor_castello <viccasgom@alu.edu.gva.es>
2025-02-09 13:47:56 +0100

    Modificacion de la clase principal

--- PruebaGitHubSTS/src/main/Main.java
+++ PruebaGitHubSTS/src/main/Main.java
@@ -9,8 +9,12 @@ public class Main {
 		ObjectA objetoA = new ObjectA(1, "Pepito", "Grillo");
 		ObjectB objetoB = new ObjectB(1, "Alfredo", 25);
 		ObjectC objetoC = new ObjectC(1, "Ana", "Valencia");
+		
 		System.out.println(objetoA.toString());
 		System.out.println(objetoB.toString());
 		System.out.println(objetoC.toString());
+		
+		ObjectC objetoC2 = new ObjectC(2, "Antonia", "Almeria");
+		System.out.println(objetoC2.toString());
 	}
 }
