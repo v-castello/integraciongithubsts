5ca90aea58302bb9994441131930fcec2ac4438f
victor_castello <viccasgom@alu.edu.gva.es>
2025-02-09 13:26:13 +0100

    Primera subida del proyecto

---
+++ PruebaGitHubSTS/src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import entities.ObjectA;
+import entities.ObjectB;
+import entities.ObjectC;
+
+public class Main {
+	public static void main(String[] args) {
+		ObjectA objetoA = new ObjectA(1, "Pepito", "Grillo");
+		ObjectB objetoB = new ObjectB(1, "Alfredo", 25);
+		ObjectC objetoC = new ObjectC(1, "Ana", "Valencia");
+		System.out.println(objetoA.toString());
+		System.out.println(objetoB.toString());
+		System.out.println(objetoC.toString());
+	}
+}
